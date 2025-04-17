a2cfc25a86ab06ca4c8db71765ff3de06ae74db9
zemanpeter <zeman.peter.sk@gmail.com>
2025-04-17 19:35:00 +0000

    Deploying to gh-pages from @ zemanpeter/zemanpeter.github.io@e776071d4f1886585819261a5ed27de3bd514ac3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,7 +23,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/blog/";
           },
-        },{id: "post-quadratic-equation",
+        },{id: "post-graham-pollak-determinant-formula",
+        
+          title: "Graham-Pollak determinant formula",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/graham-pollak/";
+          
+        },
+      },{id: "post-quadratic-equation",
         
           title: "Quadratic equation",
         
