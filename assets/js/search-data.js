10656c26bd15c77547c64e02177b1207f2687f5a
zemanpeter <zeman.peter.sk@gmail.com>
2025-04-26 15:08:13 +0000

    Deploying to gh-pages from @ zemanpeter/zemanpeter.github.io@f795ac6e0ed4a4d3c866d64ac4d34c3a711ad62c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -70,14 +70,14 @@ ninja.data = [{
           section: "News",},{id: "news-i-gave-a-talk-at-algebra-colloqium-about-our-npa-hierarchy-for-quantum-isomorphism-and-homomorphism-indistinguishability-paper-see-the-slides",
           title: 'I gave a talk at Algebra colloqium about our NPA Hierarchy for Quantum...',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-quantum-information-theory",
+          title: 'Quantum Information Theory',
+          description: "Course on quantum information theory.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-permutation-groups",
+          title: 'Permutation groups',
+          description: "Course on permutation groups.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
