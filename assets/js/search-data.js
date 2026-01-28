94159e479bd670f739be6d870aa409d52723c444
zemanpeter <zeman.peter.sk@gmail.com>
2026-01-28 16:10:58 +0000

    Deploying to gh-pages from @ zemanpeter/zemanpeter.github.io@21d699311d5064f6cadce7f663d80a29561ca36c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-quantum-constraint-satisfaction-problems-2026",
-          title: "Quantum Constraint Satisfaction Problems 2026",
-          description: "Sattelite workshop of ICALP 2026.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/qucsps2026/";
-          },
         },{id: "nav-teaching",
           title: "teaching",
           description: "",
