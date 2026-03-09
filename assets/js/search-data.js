9073093db14f4b32014d25f787fe756f348394a6
zemanpeter <zeman.peter.sk@gmail.com>
2026-03-09 12:27:29 +0000

    Deploying to gh-pages from @ zemanpeter/zemanpeter.github.io@e7eed5057abf20d904b9fc86c648297512684339 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,6 +37,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/blog/";
           },
+        },{id: "nav-reading",
+          title: "reading",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/reading/";
+          },
         },{id: "post-infinity-of-primes",
         
           title: "Infinity of primes",
