3bee05d81712929bb2f905ca50e9ad8ae9ddcb12
zemanpeter <zeman.peter.sk@gmail.com>
2026-05-10 08:48:09 +0000

    Deploying to gh-pages from @ zemanpeter/zemanpeter.github.io@db7d232b1975aacfa4a2833c939dcf0ee49b57e7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -148,52 +148,7 @@ ninja.data = [{
           section: "News",},{id: "news-together-with-samson-abramsky-amin-karamlou-and-nihil-shah-we-will-be-organizing-an-icalp-2026-satellite-workshop-on-quantum-csps",
           title: 'Together with Samson Abramsky, Amin Karamlou and Nihil Shah, we will be organizing...',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
-            },},{id: "teachings-introduction-to-the-complexity-of-csp",
+          section: "News",},{id: "teachings-introduction-to-the-complexity-of-csp",
           title: 'Introduction to the complexity of CSP',
           description: "NMAG563",
           section: "Teachings",handler: () => {
